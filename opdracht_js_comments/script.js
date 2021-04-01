75bbbabe7eb933fd9c26f66fc19cdbd1a3aaaed8
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-01 13:44:22 +0200

    Add files via upload

---
+++ opdracht_js_comments/script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy studenten")
+
+//dit is een grote som
+// let sum = 1230941 * 1823794;
+// console.log(sum)
+
+// let sum1 = 637263 / 54;
+// console.log(sum1)
+
+
+
